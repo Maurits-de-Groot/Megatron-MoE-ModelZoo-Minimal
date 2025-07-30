3bc6afb31bb07d9276417b8c7724febbb79bf48b
Maurits de Groot <maurits.degroot@live.nl>
2025-07-30 08:44:46 +0000

    added README

--- Mixtral-8x22B/download_mixtral.py
+++
@@ -1,7 +0,0 @@
-import os
-from huggingface_hub import snapshot_download
-WORKDIR = os.environ["WORKDIR"]
-SAVED_DIR=f"{WORKDIR}/hf_checkpoint"
-
-# Download HF checkpoints
-snapshot_download(repo_id="mistralai/Mixtral-8x22B-v0.1", ignore_patterns=["*.pt"], local_dir=SAVED_DIR, local_dir_use_symlinks=False)
